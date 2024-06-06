69673331e743d9220861356201a31d0de2bd7eb3
Puneet Shrivastava <puneet.shrivastava@ymail.com>
2024-06-06 20:46:47 +0530

    First Commit

---
+++ src/test/java/com/puneet/spring_jenkins/SpringJenkinsApplicationTests.java
@@ -0,0 +1,21 @@
+package com.puneet.spring_jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class SpringJenkinsApplicationTests {
+
+	public static Logger logger = LoggerFactory.getLogger("SpringJenkinsApplicationTests") ;
+
+	@Test
+	void contextLoads() {
+	logger.info("Test Case Executing ....");
+		assertEquals("true","true") ;
+	}
+
+}
